89954bdad0e7499a059bf0d28af4310793590aca
Iina Hirvonen <iinahirvonen@MacBook-Air.local>
2026-05-07 11:10:58 +0300

    Lisätty kertolasku-funktio

--- laskin.py
+++ laskin.py
@@ -3,6 +3,9 @@ def yhteenlasku(a, b):
     return a + b
 def vähennyslasku(a, b):
     return a - b
+def kertolasku(a, b):
+    return a * b
 print ("Laskin vl1.0")
 print("5 + 3 =", yhteenlasku(5, 3))
 print("10 - 4 =", vähennyslasku(10, 4))
+print("6 * 7 ="), kertolasku(6, 7)
