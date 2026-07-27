bff7f86e41bf8946d8fb8de4a4d9bf0dcd9c67a6
Innu-x <iina_92@hotmail.com>
2026-07-27 08:26:31 +0300

    Remove potenssilasku function from laskin.py
    Removed the potenssilasku function and its usage.

--- laskin.py
+++ laskin.py
@@ -6,9 +6,7 @@ def vähennyslasku(a, b):
 def kertolasku(a, b):
     return a * b
 def jakolasku (a, b):
-    return a / b
-def potenssilasku(a, b): 
-    return a ** b      
+    return a / b  
 def plusmiinuslasku(a, b, c):
     return a + b - c
 print ("Laskin vl1.0")
@@ -17,4 +15,4 @@ print ("10 - 4 =", vähennyslasku(10, 4))
 print ("6 * 7 =", kertolasku(6, 7))
 print ("8 / 4 =", jakolasku(8, 4))
 print("5 + 3 - 2 =", plusmiinuslasku(5, 3, 2))
-print("2 ^ 8 =", potenssilasku(2, 8))
+
