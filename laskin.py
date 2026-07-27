ac363ce5d0ef3c1f34dda5391b1d0ea53bf36b71
Innu-x <282511576+Innu-x@users.noreply.github.com>
2026-07-27 08:38:50 +0300

    Lisätty potenssilasku-funktio

--- laskin.py
+++ laskin.py
@@ -9,10 +9,12 @@ def jakolasku (a, b):
     return a / b  
 def plusmiinuslasku(a, b, c):
     return a + b - c
+def potenssilasku(a, b):
+    return a ** b
 print ("Laskin vl1.0")
 print ("5 + 3 =", yhteenlasku(5, 3))
 print ("10 - 4 =", vähennyslasku(10, 4))
 print ("6 * 7 =", kertolasku(6, 7))
 print ("8 / 4 =", jakolasku(8, 4))
-print("5 + 3 - 2 =", plusmiinuslasku(5, 3, 2))
-
+print ("5 + 3 - 2 =", plusmiinuslasku(5, 3, 2))
+print ("2 ** 8 =", potenssilasku(2, 8))
